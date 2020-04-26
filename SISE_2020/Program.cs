6fb8b0123f4b547d32658978806dc03bdc709ddd
Filip Mazurek <f.maz.98@outlook.com>
2020-04-26 14:26:04 +0200

    Created base fifteen puzzle matrix structure

--- SISE_2020/Program.cs
+++ SISE_2020/Program.cs
@@ -6,12 +6,39 @@ using System.Threading.Tasks;
 
 namespace SISE_2020
 {
+    /// <summary>
+    /// Program handling fifteen puzzle with three differrent algorithms
+    /// </summary>
+    // TODO: Proper input arguments
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.ReadKey();
+            string algorithm = args[0];
+            string strategy = args[1];
+            string input = System.IO.File.ReadAllText(args[2]);
+            char[] separators = { ' ', '\n', '\r' };
+            string[] inputValues = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            string outputSolutionPath = args[3];
+            string outputStatsPath = args[4];
+
+            switch (algorithm)
+            {
+                case "bfs":
+
+                    break;
+
+                case "dfs":
+
+                    break;
+
+                case "astr":
+
+                    break;
+            }
+
+            PuzzleMatrix testMatrix = new PuzzleMatrix(inputValues);
+            Console.WriteLine(testMatrix.ToString());
         }
     }
 }
---
+++ SISE_2020/PuzzleMatrix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISE_2020
+{
+    class PuzzleMatrix
+    {
+        private int[,] matrix;
+
+        public PuzzleMatrix(int x, int y, int[] values)
+        {
+            matrix = new int[y, x];
+            if (values.Length == matrix.Length)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    matrix[i / 4, i % 4] = values[i];
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Generated puzzle matrix has size {values.Length} while provided values had size {matrix.Length}. Aborting.");
+            }
+        }
+
+        public PuzzleMatrix(string[] values)
+        {
+            int rows = int.Parse(values[0]);
+            int columns = int.Parse(values[1]);
+            matrix = new int[rows, columns];
+            if (values.Length - 2 == matrix.Length)
+            {
+                for (int i = 0; i < values.Length - 2; i++)
+                {
+                    matrix[i / 4, i % 4] = int.Parse(values[i + 2]);
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Generated puzzle matrix has size {values.Length} while provided values had size {matrix.Length}. Aborting.");
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder($"Puzzle Matrix. Size: {matrix.GetUpperBound(0) + 1}x{matrix.GetUpperBound(1) + 1}\n");
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                output.Append(matrix[i / 4, i % 4].ToString());
+                if ((i + 1) % 4 == 0 && i != 0)
+                {
+                    output.Append("\n");
+                }
+                else
+                {
+                    output.Append("\t");
+                }
+            }
+            return output.ToString();
+        }
+    }
+}
